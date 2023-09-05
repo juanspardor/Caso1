bcb7e480ba8fa4f83b7cc6d2c06d7b3c4780ff57
juanspardor <j.pardor@uniandes.edu.co>
2023-09-04 19:23:55 -0500

    version final ciclo de productor

--- src/Clases/Productor.java
+++ src/Clases/Productor.java
@@ -19,21 +19,15 @@ public class Productor extends Thread
 	
 	public void run()
 	{
-		//Nos permite terminar los productores apenas terminen de producir los suyos, no implica una vuelta extra
-//		for(int i = 0; i<cantidadAProducir; i++)
-//		{
-//			producto = new Producto(contador.aumentarProducidos(), id);
-//			bodega.almacenar(producto);	//Este tiene un sync en clase Bodega (buffer)
-//			producto.dormir(); //Este tiene un sync en la clase Producto
-//		}
-//		
-		//Nos permite que se produzca un producto siempre y cuando hayan productos por producir y algun productor este despierto
-		while(!contador.pararBusqueda())
+//		Nos permite terminar los productores apenas terminen de producir los suyos, no implica una vuelta extra
+		for(int i = 0; i<cantidadAProducir; i++)
 		{
 			producto = new Producto(contador.aumentarProducidos(), id);
 			bodega.almacenar(producto);	//Este tiene un sync en clase Bodega (buffer)
 			producto.dormir(); //Este tiene un sync en la clase Producto
 		}
+		
+		
 		System.out.println("Termino el productor "+id);
 	}
 	
